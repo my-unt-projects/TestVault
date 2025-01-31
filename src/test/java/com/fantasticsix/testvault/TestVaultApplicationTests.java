0bfefbfdc2f997645970bd89ac1b456d75bb4620
Md Afikur Rahman Khan <MdAfikurRahmanKhan@my.unt.edu>
2025-01-30 21:48:04 -0600

    init commit

---
+++ src/test/java/com/fantasticsix/testvault/TestVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantasticsix.testvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
