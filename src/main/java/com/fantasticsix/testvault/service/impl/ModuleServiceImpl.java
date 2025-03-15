2f484684a43994c3b9b10a21719768d7821a96af
Arup Datta <arupdatta@my.unt.edu>
2025-03-14 21:30:32 -0500

    Add service and repository methods for module and test case

---
+++ src/main/java/com/fantasticsix/testvault/service/impl/ModuleServiceImpl.java
@@ -0,0 +1,51 @@
+package com.fantasticsix.testvault.service.impl;
+
+import com.fantasticsix.testvault.model.Project;
+import com.fantasticsix.testvault.repository.ModuleRepository;
+import com.fantasticsix.testvault.repository.ProjectRepository;
+import com.fantasticsix.testvault.service.ModuleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import com.fantasticsix.testvault.model.Module;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ModuleServiceImpl implements ModuleService {
+    private final ModuleRepository moduleRepository;
+    private final ProjectRepository projectRepository;
+
+    @Override
+    public Module add(com.fantasticsix.testvault.model.Module module) {
+        return moduleRepository.save(module);
+    }
+
+    @Override
+    public Module get(Long id) {
+        return moduleRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Module with id " + id + " not found"));
+    }
+
+    @Override
+    public Module update(Module module) {
+        Module savedModule = moduleRepository.findById(module.getModuleId())
+                .orElseThrow(() -> new RuntimeException("Module with id " + module.getModuleId() + " not found"));
+        savedModule.setModuleName(module.getModuleName());
+        savedModule.setDescription(module.getDescription());
+        savedModule.setTestCases(module.getTestCases());
+        return moduleRepository.save(savedModule);
+    }
+
+    @Override
+    public void delete(Long id) {
+        moduleRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Module> getByProjectId(Long projectId) {
+        Project project = projectRepository.findById(projectId)
+                .orElseThrow(() -> new RuntimeException("Project with id " + projectId + " not found"));
+        return moduleRepository.getModulesByProject(project);
+    }
+}
---
+++ src/main/java/com/fantasticsix/testvault/service/impl/TestCaseServiceImpl.java
@@ -0,0 +1,55 @@
+package com.fantasticsix.testvault.service.impl;
+
+
+import com.fantasticsix.testvault.model.Module;
+import com.fantasticsix.testvault.model.TestCase;
+import com.fantasticsix.testvault.repository.ModuleRepository;
+import com.fantasticsix.testvault.repository.TestCaseRepository;
+import com.fantasticsix.testvault.service.TestCaseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TestCaseServiceImpl implements TestCaseService {
+    private final TestCaseRepository testCaseRepository;
+    private final ModuleRepository moduleRepository;
+
+    @Override
+    public TestCase get(Long id) {
+        return testCaseRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("TestCase with id " + id + " not found"));
+    }
+
+    @Override
+    public TestCase save(TestCase testCase) {
+        return testCaseRepository.save(testCase);
+    }
+
+    @Override
+    public void delete(Long id) {
+        testCaseRepository.deleteById(id);
+    }
+
+    @Override
+    public TestCase update(TestCase testCase) {
+        TestCase savedTestCase = testCaseRepository.findById(testCase.getTestCaseId())
+                .orElseThrow(() -> new RuntimeException("TestCase with id " + testCase.getTestCaseId() + " not found"));
+        savedTestCase.setAssignedTo(testCase.getAssignedTo());
+        savedTestCase.setDescription(testCase.getDescription());
+        savedTestCase.setDueDate(testCase.getDueDate());
+        savedTestCase.setPriority(testCase.getPriority());
+        savedTestCase.setStatus(testCase.getStatus());
+        return testCaseRepository.save(savedTestCase);
+    }
+
+    @Override
+    public List<TestCase> getByModuleId(Long moduleId) {
+        Module module = moduleRepository.findById(moduleId)
+                .orElseThrow(() -> new RuntimeException("Module not found"));
+
+        return testCaseRepository.getTestCasesByModule(module);
+    }
+}
