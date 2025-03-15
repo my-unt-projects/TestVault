e7fedeecbc72b42dfc94925886733e9f8172639f
Arup Datta <arupdatta@my.unt.edu>
2025-03-15 15:48:48 -0500

    Add delete module functionality from the ui

--- src/main/java/com/fantasticsix/testvault/controller/ModuleController.java
+++ src/main/java/com/fantasticsix/testvault/controller/ModuleController.java
@@ -9,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -71,5 +68,10 @@ public class ModuleController {
         return "redirect:/modules";
     }
 
+    @GetMapping("/delete/{id}")
+    public String deleteModule(@PathVariable Long id) {
+        moduleService.delete(id);
+        return "redirect:/modules";
+    }
 
 }
