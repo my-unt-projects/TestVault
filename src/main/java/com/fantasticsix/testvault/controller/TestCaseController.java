9c1fc7240ba205fd68a0f82aba8aa69a0b9ee848
jarintasnimishika121 <jarintasnimishika@my.unt.edu>
2025-03-16 13:02:51 -0500

    Create some part of Testcase Controller

--- src/main/java/com/fantasticsix/testvault/controller/TestCaseController.java
+++ src/main/java/com/fantasticsix/testvault/controller/TestCaseController.java
@@ -1,22 +1,60 @@
+
+
 package com.fantasticsix.testvault.controller;
 
-import com.fantasticsix.testvault.constants.ProjectStatus;
-import com.fantasticsix.testvault.model.Project;
 import com.fantasticsix.testvault.model.TestCase;
-import com.fantasticsix.testvault.model.User;
+import com.fantasticsix.testvault.service.TestCaseService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/tests")
+@RequiredArgsConstructor
 public class TestCaseController {
+    private final TestCaseService testCaseService;
+
     @GetMapping("/new")
     public String showCreateForm(Model model) {
         model.addAttribute("testCase", new TestCase());
         return "tests/create";
     }
-}
+    @PostMapping
+    public String createTestCase(@ModelAttribute TestCase testCase) {
+        testCaseService.createTestCase(testCase);
+        return "redirect:/tests/all";
+    }
+
+    @GetMapping("/all")
+    public String getAllTestCases(Model model) {
+        List<TestCase> testCases = testCaseService.listOfAllTestCases();
+        model.addAttribute("testCases", testCases);
+        return "tests/list";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable Long id, Model model) {
+        TestCase testCase = testCaseService.updateTestCase(id, new TestCase());
+        model.addAttribute("testCase", testCase);
+        return "tests/list";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateTestCase(@PathVariable Long id, @ModelAttribute TestCase testCase) {
+        testCaseService.updateTestCase(id, testCase);
+        return "redirect:/tests/all";
+    }
+
+    @GetMapping("/delete/{title}")
+    public String deleteTestCase(@PathVariable String title) {
+        testCaseService.deleteTestCaseByTitle(title);
+        return "redirect:/tests/all";
+    }
+
+
+    }
+
+
