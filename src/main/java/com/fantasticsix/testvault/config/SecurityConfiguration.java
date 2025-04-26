8d49e775c057ac69ede9f8a1d67f337802e86c26
Md Afikur Rahman Khan <MdAfikurRahmanKhan@my.unt.edu>
2025-04-26 13:11:55 -0500

    enable method level security

--- src/main/java/com/fantasticsix/testvault/config/SecurityConfiguration.java
+++ src/main/java/com/fantasticsix/testvault/config/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -21,6 +22,7 @@ import java.util.Optional;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     @Bean
