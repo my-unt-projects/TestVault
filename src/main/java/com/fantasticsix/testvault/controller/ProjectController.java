fa67ae6d1cf4817bac613bccee089d5cf7b57eac
Md Afikur Rahman Khan <MdAfikurRahmanKhan@my.unt.edu>
2025-04-26 12:34:13 -0500

    fix manager role for project creation

--- src/main/java/com/fantasticsix/testvault/controller/ProjectController.java
+++ src/main/java/com/fantasticsix/testvault/controller/ProjectController.java
@@ -30,7 +30,7 @@ public class ProjectController {
 
     @GetMapping("/new")
     public String showCreateForm(Model model) {
-        List<User> managers = userRepository.findByRoleName("ROLE_ADMIN");  // TODO: Change to ROLE_MANAGER
+        List<User> managers = userRepository.findByRoleName("ROLE_MANAGER");
         model.addAttribute("title", "Create Project");
         model.addAttribute("project", new Project());
         model.addAttribute("statusOptions", ProjectStatus.values());
